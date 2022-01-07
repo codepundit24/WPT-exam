9e687799dbf0486d6c79f3cad97ea855f45cfd5e
codepundit24 <sharmaanurag897@gmail.com>
2022-01-07 18:07:01 +0530

    ReactFile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import axios from "axios";
+import { useState } from "react";
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    < Myapp/>
+    </>
   );
 }
 
-export default App;
+function Myapp(){
+  const [applicationName, setApplicationName ] = useState("");
+  const [studentName , setStudentName] = useState("");
+  const [studentId, setStudentId] = useState("");
+  const [list,setList ]= useState([]);
+
+  const handleApplicationNameChange = (e) => {
+    setApplicationName(e.target.value);
+  }
+
+  const handleStudentNamechange = (e) => {
+    setStudentName(e.target.value);
+  }
+
+  const handleStudentIdchange = (e) => {
+    setStudentId(e.target.value);
+  }
+
+  const addUser = async () => {
+    const url = "http://localhost:4000/1";
+    const data = {
+      applicationName: applicationName,
+      studentName:studentName,
+      studentId:studentId,
+    };
+   await axios.post(url,data);
+
+   const newList = [data, ...list];
+   setList(newList);
+  }
+
+  const getUser = async () => {
+    const url = "http://localhost:4000/2";
+  
+   const result = await axios.get(url);
+   const list = result.data;
+   const newList = [ ...list];
+   setList(newList);
+  };
+
+return (
+  <div>
+  <div >
+    <h1 >
+      <input 
+    type="text" 
+    value={applicationName} 
+     onChange={handleApplicationNameChange}
+    />
+     </h1>
+     <h4> <input 
+    type="text" 
+    value={applicationName} 
+     onChange={handleStudentNamechange}
+    /></h4>
+    <h4>
+    <input 
+    type="text" 
+    value={applicationName} 
+     onChange={handleStudentIdchange}
+    />
+    </h4>
+  </div>
+  <div>
+    <input type="text" placeholder="Let's chat here"/>
+    <input type="button" placeholder="SEND" onClick={getUser}/>
+  </div>
+  <div>
+    {list.map((item,index) =>(
+      <div key={index}>
+        {item.applicationName}{item.studentName}{item.studentId}
+        </div>
+    ))}
+  </div>
+  </div>
+);
+};
